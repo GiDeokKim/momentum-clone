d48cdd7b177e1024512071d251b0d4c8c8580b12
김기덕 <ikid4127@naver.com>
2023-09-17 23:38:12 +0900

    change styles

--- js/background.js
+++ js/background.js
@@ -1,8 +1,4 @@
-const images = [
-    "0.jpg",
-    "1.jpg",
-    "2.jpg",
-]
+const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
@@ -10,4 +6,9 @@ const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+// document.body.appendChild(bgImage);
+
+document.body.style.backgroundImage = `url(${bgImage.src})`;
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundPosition = "center";
+document.body.style.backgroundRepeat = "no-repeat";
